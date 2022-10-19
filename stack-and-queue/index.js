acda6d42bd77d8ff392d8efe490eb8270f9554bc
HelloRave <huang.weiwei@trentglobal.edu.sg>
2022-10-19 14:00:33 +0000

    Completed stack and queue

---
+++ stack-and-queue/index.js
@@ -0,0 +1,82 @@
+class Node {
+    constructor(val) {
+        this.value = val;
+        this.next = null;
+    }
+}
+
+class Stack {
+    constructor() {
+        this.first = null;
+        this.last = null;
+        this.size = 0;
+    }
+
+    push(val) {
+        let newNode = new Node(val);
+        if (!this.first) {
+            this.first = newNode;
+            this.last = newNode;
+        } else {
+            let temp = this.first;
+            this.first = newNode;
+            this.first.next = temp;
+        }
+        return this.size++;
+    }
+
+    pop() {
+        if (!this.first) return null;
+        let temp = this.first;
+        if (this.first === this.last) {
+            this.last = null;
+        }
+        this.first = this.first.next;
+        this.size--;
+        return temp.value;
+    }
+}
+
+class Queue {
+    constructor() {
+        this.first = null;
+        this.last = null;
+        this.size = 0;
+    }
+
+    enqueue(val) {
+        let newNode = new Node(val);
+        if (!this.first) {
+            this.first = newNode;
+            this.last = newNode;
+        } else {
+            this.last.next = newNode;
+            this.last = newNode;
+        }
+        return ++this.size;
+    }
+
+    dequeue() {
+        if (!this.first) return null;
+        let temp = this.first;
+        if (this.first === this.last) {
+            this.last = null;
+        }
+        this.first = this.first.next;
+        this.size--;
+        return temp.value;
+    }
+}
+
+let stack = new Stack();
+let queue = new Queue();
+stack.push(25)
+stack.push(30)
+stack.push(50)
+console.log(stack.pop())
+
+queue.enqueue(11)
+queue.enqueue(12)
+queue.enqueue(13)
+console.log(queue)
+console.log(queue.dequeue())
